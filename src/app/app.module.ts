cbaa658be4b729d081525cb5856a78f2ab26fffe
Spencer Cheng <spencercheng@Spencers-MacBook-Pro.local>
2018-09-24 19:04:43 -0500

    landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,57 @@
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { TeamComponent } from './components/team/team.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+
+const appRoutes: Routes = [
+
+  {
+    path: 'landing',
+    component: LandingComponent,
+    data: { title: 'Landing' }
+  },
+  {
+    path:'about',
+    component:AboutComponent,
+    data: {title: 'About'}
+  },
+  {
+    path:'team',
+    component:TeamComponent,
+    data: {title: 'Team'}
+  },
+  {
+    path:'contact',
+    component:ContactComponent,
+    data: {title: 'Contact'}
+  },
+  { path: '',
+    redirectTo: '/landing',
+    pathMatch: 'full'
+  },
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    TeamComponent,
+    ContactComponent,
+    LandingComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
